b6810a115ab3bff06f7f4d5dbc75c3e9f92993d8
神楽坂雅詩 <cxchope@gmail.com>
2013-08-27 18:19:47 +0800

    更新下载到本地入口

--- yashi0-5y_edgeActions.js
+++ yashi0-5y_edgeActions.js
@@ -243,6 +243,12 @@ var Composition = Edge.Composition, Symbol = Edge.Symbol; // aliases for commonl
       });
       //Edge binding end
 
+      Symbol.bindElementAction(compId, symbolName, "${_Text}", "click", function(sym, e) {
+         window.open("https://github.com/cxchope/KagurazakaYashi0.5StAnniversary", "_new");
+
+      });
+      //Edge binding end
+
    })("stage");
    //Edge symbol end:'stage'
 
